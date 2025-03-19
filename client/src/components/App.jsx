1037853c19b7b5222c8e7610aec9e8d33835b595
szymonMCS <szymon.maciejewski.programista@gmail.com>
2025-03-19 19:05:48 +0100

    added all frontend templates without mechanics

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home";
+import Register from "./Register";
+import Login from "./Login";
+import Notebook from "./Notebook";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/notebook" element={<Notebook />} />
       </Routes>
     </Router>
   );
